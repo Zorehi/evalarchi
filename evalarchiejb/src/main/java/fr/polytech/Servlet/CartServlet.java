bd9610821bbbdabe71ecb02a6f09b5591dd75b21
CyrilJack <jacques.cyril80@gmail.com>
2024-04-17 13:26:53 +0200

    Début structure servlet

---
+++ evalarchiejb/src/main/java/fr/polytech/Servlet/CartServlet.java
@@ -0,0 +1,23 @@
+package fr.polytech.Servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name="Cart", urlPatterns = {"/Cart"})
+public class CartServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ evalarchiejb/src/main/java/fr/polytech/Servlet/LoginServlet.java
@@ -0,0 +1,26 @@
+package fr.polytech.Servlet;
+
+import fr.polytech.business.UserBusiness;
+import fr.polytech.business.UserBusinessImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name="Login", urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+    private UserBusiness userBusiness;
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
