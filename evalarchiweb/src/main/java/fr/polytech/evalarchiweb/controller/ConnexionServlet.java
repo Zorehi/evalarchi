258640798c474a47c1aee0e98890a84f3046e25d
Legrix Jeremy <jeremy.legrix@hotmail.com>
2024-04-17 13:25:26 +0200

    mise en place de la séparation des module

---
+++ evalarchiweb/src/main/java/fr/polytech/evalarchiweb/controller/ConnexionServlet.java
@@ -0,0 +1,17 @@
+package fr.polytech.evalarchiweb.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "connexion", urlPatterns = {"/connexion"})
+public class ConnexionServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("connexion.jsp").forward(req, resp);
+    }
+}
---
+++ evalarchiweb/src/main/webapp/connexion.jsp
@@ -0,0 +1,20 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Eval Archi</title>
+</head>
+<body>
+  <form action="connexion" method="post">
+    <div>
+      <label>Login</label>
+      <input type="text" name="login">
+    </div>
+    <div>
+      <label>Mot de passe</label>
+      <input type="password" name="password">
+    </div>
+    <button type="submit">Connexion</button>
+  </form>
+</body>
+</html>
